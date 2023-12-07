29db7d89852a62be5bc9e5a788396645c18c0a5a
Md Forhad Sarkar <ashadullahalforhad@gmail.com>
2023-12-07 09:10:49 +0600

    edit design added

--- src/pages/CalendarOld.tsx
+++ src/pages/CalendarOld.tsx
@@ -43,55 +43,21 @@ const Calendar = () => {
           <tbody>
             {/* <!-- Line 1 --> */}
             <tr className="grid grid-cols-7">
-              <td className="ease relative h-20 border border-stroke p-2md:h-25 md:p-6 xl:h-31">
-                <div className="grid gap-4 grid-rows-2">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <span className="font-medium text-black dark:text-white">
-                        1
-                      </span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 0 24 24"
-                        width="24px"
-                        fill="#fcca03"
-                      >
-                        <path d="M0 0h24v24H0z" fill="none" />
-                        <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z" />
-                      </svg>
-                    </div>
-                    <span className="ml-2 cursor-pointer transition duration-500 hover:bg-gray dark:border-strokedark dark:hover:bg-meta-4">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 0 24 24"
-                        width="24px"
-                        fill="#037ffc"
-                      >
-                        <path d="M0 0h24v24H0z" fill="none" />
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
-                      </svg>
+              <td className="ease relative h-20 cursor-pointer border border-stroke p-2 transition duration-500 hover:bg-gray dark:border-strokedark dark:hover:bg-meta-4 md:h-25 md:p-6 xl:h-31">
+                <span className="font-medium text-black dark:text-white">
+                  1
+                </span>
+                <div className="group h-16 w-full flex-grow cursor-pointer py-1 md:h-30">
+                  <span className="group-hover:text-primary md:hidden">
+                    More
+                  </span>
+                  <div className="event invisible absolute left-2 z-99 mb-1 flex w-[200%] flex-col rounded-sm border-l-[3px] border-primary bg-gray px-3 py-1 text-left opacity-0 group-hover:visible group-hover:opacity-100 dark:bg-meta-4 md:visible md:w-[190%] md:opacity-100">
+                    <span className="event-name text-sm font-semibold text-black dark:text-white">
+                      Redesign Website
                     </span>
-                  </div>
-
-                  <div className="event invisible left-1 z-99 mb-1 flex w-full flex-col rounded-sm border-l-[3px] border-primary bg-gray px-3 py-1 text-left opacity-0 dark:bg-meta-4 md:visible md:opacity-100">
-                    <div className="flex items-center justify-between">
-                      <span className="event-name text-sm font-semibold text-black dark:text-white">
-                        50 MT{' '}
-                      </span>
-                      <span className="ml-2 cursor-pointer transition duration-500 hover:bg-gray dark:border-strokedark dark:hover:bg-meta-4">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          height="24"
-                          viewBox="0 -960 960 960"
-                          width="24"
-                          fill="#818583"
-                        >
-                          <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
-                        </svg>
-                      </span>
-                    </div>
+                    <span className="time text-sm font-medium text-black dark:text-white">
+                      1 Dec - 2 Dec
+                    </span>
                   </div>
                 </div>
               </td>
@@ -234,9 +200,9 @@ const Calendar = () => {
                     <span className="event-name text-sm font-semibold text-black dark:text-white">
                       App Design
                     </span>
-                    {/* <span className="time text-sm font-medium text-black dark:text-white">
+                    <span className="time text-sm font-medium text-black dark:text-white">
                       25 Dec - 27 Dec
-                    </span> */}
+                    </span>
                   </div>
                 </div>
               </td>
